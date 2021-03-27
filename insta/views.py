31a3b0960c51272ebb8881660847f5824c018a93
atsalibram <atsalibaram@gmail.com>
2021-03-27 11:30:00 +0300

    created url.py for insta

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def homepage (request):
+    current_user = request.user
+    all_images = Image.objects.all()
+    comments = Comments.objects.all()
+    likes = Likes.objects.all()
+    profile = Profile.objects.all()
+    print(likes)
+    return render(request,'home.html',locals())
