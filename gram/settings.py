e42506b555bca02cc9919144be88f5508990b3d8
atsalibram <atsalibaram@gmail.com>
2021-04-03 17:47:54 +0300

    pushing to heroku

--- gram/settings.py
+++ gram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
  #Quick-start development settings - unsuitable for production
  #See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'o^#zqrb=warnyiji59g25e-*u4o1va(543j%q*5%xfd3)^gyjz'
+SECRET_KEY = 'o^#zqrb=warnyiji59g25e-*u4o1va(543j%q*5%xfd3)^gyjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,39 +73,39 @@ TEMPLATES = [
         },
     },
 ]
-
+#
 WSGI_APPLICATION = 'gram.wsgi.application'
-
-
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
- #development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
-       
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-# Database
+#
+#
+#MODE=config("MODE", default="dev")
+#SECRET_KEY = config('SECRET_KEY')
+#DEBUG = config('DEBUG', default=False, cast=bool)
+# #development
+#if config('MODE')=="dev":
+#   DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#           'NAME': config('DB_NAME'),
+#           'USER': config('DB_USER'),
+#           'PASSWORD': config('DB_PASSWORD'),
+#           'HOST': config('DB_HOST'),
+#           'PORT': '',
+#       }
+#       
+#   }
+## production
+#else:
+#   DATABASES = {
+#       'default': dj_database_url.config(
+#           default=config('DATABASE_URL')
+#       )
+#   }
+#
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
+#
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+## Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
