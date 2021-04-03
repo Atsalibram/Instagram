66359d1c2a6a9c5787177685d88b00b1eead08c7
atsalibram <atsalibaram@gmail.com>
2021-04-03 17:35:25 +0300

    pushing to heroku

--- gram/settings.py
+++ gram/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'insta.apps.InstaConfig',
     'tinymce',
     'insta',
-    'decouple',
+   
 
 
 ]
@@ -154,13 +153,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
